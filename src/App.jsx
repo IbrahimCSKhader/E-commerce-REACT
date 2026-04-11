fc2dba09f326bea84ccb764a2ccc458c3ec684d0
IbrahimCSKhader <ik2907951@gmail.com>
2026-04-11 19:55:01 +0300

    reomve the bad library

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import router from "./rout.jsx";
 import AuthContextProvider from "./context/AuthContext.jsx";
